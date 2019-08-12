913e8942421e8942fbcee4ff00c15acddb1b9a39
Candice Chin <chincandice2007@gmail.com>
2019-08-11 20:59:17 -0700

    another set of attemps

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,20 +1,131 @@
 
+//sets up random number user is trying to match
+$(document).ready(function(){
+    var Random=Math.floor(Math.random()*101+19)
+    console.log(Random)
+    $('#number').text(Random);
+
+//	setting up four random numbers array, total variable and printing the wins & losses variables	
+var GeneratedNumbers = []
+var total= 0;	
+var wins= 0;
+var losses = 0;
+$('#numberWins').text(wins);
+$('#numberLosses').text(losses);		
+
+
+//sets up random numbers for each jewel
+function fourRandomNumbers(){
+for (var i = 0; i < 4; i++) {
+    var num = Math.floor(Math.random()*11+1);
+    GeneratedNumbers.push(num);
+}
+console.log(GeneratedNumbers) 
+}
+
+fourRandomNumbers();
+
+
+//resets the game
+function reset(){
+    Random=Math.floor(Math.random()*101+19);
+    console.log(Random)
+    $('#number').text(Random);
+    GeneratedNumbers = [];
+    fourRandomNumbers();
+    total= 0;
+    $('#Combined').text(total);
+    } 
+
+//adds the wins to the total
+function yay(){
+alert("You won!!!");
+    wins++;	
+    $('#numberWins').text(wins);
+    reset();
+}
+
+//adds the losses to the total
+function loser(){
+alert ("You lose!!");
+    losses++;
+    $('#numberLosses').text(losses);
+    reset();
+}
+
+
+
+//sets up click for jewels
+    $('#one').on ('click', function(){
+        total = total + GeneratedNumbers[0];
+        console.log("New total= " + total);
+        $('#Combined').text(total);	
+
+                    //sets win/lose conditions
+                if (total == Random){
+                    yay();
+                }
+                else if ( total > Random){
+                    loser();
+                }		
+    })	
+
+    $('#two').on ('click', function(){
+        total = total + GeneratedNumbers[1];
+        console.log("New total= " + total);
+        $('#Combined').text(total);	
+
+                if (total == Random){
+                    yay();
+                }
+                else if ( total > Random){
+                    loser();
+                }	
+    })	
+
+    $('#three').on ('click', function(){
+        total = total + GeneratedNumbers[2];
+        console.log("New total= " + total);
+        $('#Combined').text(total);
+
+//sets win/lose conditions
+                    if (total == Random){
+                    yay();
+                }
+                else if ( total > Random){
+                    loser();
+                }	
+    })	
+
+    $('#four').on ('click', function(){
+        total = total + GeneratedNumbers[3];
+        console.log("New total= " + total);
+        $('#Combined').text(total);	
+
+            
+                    if (total == Random){
+                    yay();
+                }
+                else if ( total > Random){
+                    loser();
+                }
+    });  	
+
+});	
+
+
+
+
+
+
 // Declare global variables
-var randomTarget;
-var losses;
-var wins;
+
 
 // Create the four crystals using for loop
-for(var i = 0; i < 4; i++) {
+
+
 // Generate a random number for each crystal, between 1 and 12
-    var random = Math.floor(Math.random() *12);
-    console.log(random);
-    var crystal = $("<div>");
-        crystal.attr("class", 'crystal');
 
-    $(".crystals").append(crystal);
-
-}
 
 
 // There will be four crystals displayed as buttons on the page.
